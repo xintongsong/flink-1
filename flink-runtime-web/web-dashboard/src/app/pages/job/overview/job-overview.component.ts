1a45d666d1d26be26bef806c2af16328ad55b6c4
simplejason <simplejason.coder@gmail.com>
2019-07-10 11:23:18 +0800

    [FLINK-13149][Web Frontend] support configuration and update Angular to 8.0

--- flink-runtime-web/web-dashboard/src/app/pages/job/overview/job-overview.component.ts
+++ flink-runtime-web/web-dashboard/src/app/pages/job/overview/job-overview.component.ts
@@ -39,7 +39,7 @@ import { DagreComponent } from 'share/common/dagre/dagre.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JobOverviewComponent implements OnInit, OnDestroy {
-  @ViewChild(DagreComponent) dagreComponent: DagreComponent;
+  @ViewChild(DagreComponent, { static: true }) dagreComponent: DagreComponent;
   nodes: NodesItemCorrectInterface[] = [];
   links: NodesItemLinkInterface[] = [];
   destroy$ = new Subject();
@@ -50,7 +50,7 @@ export class JobOverviewComponent implements OnInit, OnDestroy {
 
   onNodeClick(node: NodesItemCorrectInterface) {
     if (!(this.selectedNode && this.selectedNode.id === node.id)) {
-      this.router.navigate([node.id], { relativeTo: this.activatedRoute }).then();
+      this.router.navigate([node.id], { relativeTo: this.activatedRoute, queryParamsHandling: 'merge' }).then();
     }
   }
 
