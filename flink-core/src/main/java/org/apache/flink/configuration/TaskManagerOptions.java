e661a3e4bb0dafc3d5c96850e6005460851a1ca2
Xintong Song <tonysong820@gmail.com>
2019-10-09 17:30:23 +0800

    [FLINK-14188][runtime] Introduce config option for default slot resource fraction.

--- flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
@@ -149,8 +149,10 @@ public class TaskManagerOptions {
 
 	/**
 	 * The config parameter defining the number of task slots of a task manager.
+	 *
+	 * @deprecated use {@link #DEFAULT_SLOT_FRACTION}
 	 */
-	@Documentation.CommonOption(position = Documentation.CommonOption.POSITION_PARALLELISM_SLOTS)
+	@Deprecated
 	public static final ConfigOption<Integer> NUM_TASK_SLOTS =
 		key("taskmanager.numberOfTaskSlots")
 			.defaultValue(1)
@@ -160,6 +162,20 @@ public class TaskManagerOptions {
 				" available memory is divided between the different operator or function instances. This value" +
 				" is typically proportional to the number of physical CPU cores that the TaskManager's machine has" +
 				" (e.g., equal to the number of cores, or half the number of cores).");
+
+	/**
+	 * Fraction of TaskExecutor resources a default slot should take for slot requests with unknown resource profiles.
+	 */
+	@Documentation.CommonOption(position = Documentation.CommonOption.POSITION_PARALLELISM_SLOTS)
+	public static final ConfigOption<Float> DEFAULT_SLOT_FRACTION =
+		key("taskmanager.default-slot-fraction")
+			.defaultValue(-1.0f)
+			.withDescription("For operator or user function instances that do not specify their resource requirements,"
+				+ " we use this fraction to decide how many resources of the task executor each slot should take. A"
+				+ " default slot should have the configured fraction of the task executor resources on all resource"
+				+ " dimensions (CPU, Memory). If the fraction is not explicitly configured (or configured with negative"
+				+ " values), it will be derived from the legacy config option '"
+				+ NUM_TASK_SLOTS.key() + "'.");
 
 	public static final ConfigOption<Boolean> DEBUG_MEMORY_LOG =
 		key("taskmanager.debug.memory.log")
