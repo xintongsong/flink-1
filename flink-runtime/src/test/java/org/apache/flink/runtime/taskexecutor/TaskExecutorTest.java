azagrebin/flink

[hotfix] Refactor out slots creation from the TaskSlotTable constructor

flink-runtime/src/test/java/org/apache/flink/runtime/taskexecutor/TaskExecutorTest.java
@@ -2043,7 +2043,7 @@ public class TaskExecutorTest extends TestLogger {
 		private final Queue<SlotReport> slotReports;
 
 		private TestingTaskSlotTable(Queue<SlotReport> slotReports) {
-			super(TaskSlotTableBuilder.createDefaultSlotProfiles(1), new TimerService<>(TestingUtils.defaultExecutor(), 10000L));
+			super(TaskSlotTableBuilder.createDefaultSlots(1), new TimerService<>(TestingUtils.defaultExecutor(), 10000L));
 			this.slotReports = slotReports;
 		}
 
@@ -2060,7 +2060,7 @@ public class TaskExecutorTest extends TestLogger {
 		private AllocateSlotNotifyingTaskSlotTable(
 				TimerService<AllocationID> timerService,
 				OneShotLatch allocateSlotLatch) {
-			super(TaskSlotTableBuilder.createDefaultSlotProfiles(1), timerService);
+			super(TaskSlotTableBuilder.createDefaultSlots(1), timerService);
 			this.allocateSlotLatch = allocateSlotLatch;
 		}
 
@@ -2078,7 +2078,7 @@ public class TaskExecutorTest extends TestLogger {
 		private final CountDownLatch slotsToActivate;
 
 		private ActivateSlotNotifyingTaskSlotTable(int numberOfDefaultSlots, TimerService<AllocationID> timerService, CountDownLatch slotsToActivate) {
-			super(TaskSlotTableBuilder.createDefaultSlotProfiles(numberOfDefaultSlots), timerService);
+			super(TaskSlotTableBuilder.createDefaultSlots(numberOfDefaultSlots), timerService);
 			this.slotsToActivate = slotsToActivate;
 		}
 

xintongsong/flink-1

flink-runtime/src/test/java/org/apache/flink/runtime/taskexecutor/TaskExecutorTest.java
@@ -2008,7 +2008,7 @@ public class TaskExecutorTest extends TestLogger {
 		private final Queue<SlotReport> slotReports;
 
 		private TestingTaskSlotTable(Queue<SlotReport> slotReports) {
-			super(TaskSlotAndTableFactory.createDefaultSlotProfiles(1), new TimerService<>(TestingUtils.defaultExecutor(), 10000L));
+			super(TaskSlotAndTableFactory.createDefaultSlots(1), new TimerService<>(TestingUtils.defaultExecutor(), 10000L));
 			this.slotReports = slotReports;
 		}
 
@@ -2025,7 +2025,7 @@ public class TaskExecutorTest extends TestLogger {
 		private AllocateSlotNotifyingTaskSlotTable(
 				TimerService<AllocationID> timerService,
 				OneShotLatch allocateSlotLatch) {
-			super(TaskSlotAndTableFactory.createDefaultSlotProfiles(1), timerService);
+			super(TaskSlotAndTableFactory.createDefaultSlots(1), timerService);
 			this.allocateSlotLatch = allocateSlotLatch;
 		}
 
@@ -2043,7 +2043,7 @@ public class TaskExecutorTest extends TestLogger {
 		private final CountDownLatch slotsToActivate;
 
 		private ActivateSlotNotifyingTaskSlotTable(int numberOfDefaultSlots, TimerService<AllocationID> timerService, CountDownLatch slotsToActivate) {
-			super(TaskSlotAndTableFactory.createDefaultSlotProfiles(numberOfDefaultSlots), timerService);
+			super(TaskSlotAndTableFactory.createDefaultSlots(numberOfDefaultSlots), timerService);
 			this.slotsToActivate = slotsToActivate;
 		}
 